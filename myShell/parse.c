95876324bf84c539cc87cd81e79111d147d9bbb1
like9orphanz <stein.sam.m@gmail.com>
2015-08-29 17:16:53 -0400

    pretty close to complete.. i think

--- myShell/parse.c
+++ myShell/parse.c
@@ -2,125 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 #include "parse.h"
-/*
-Param_t * new_paramt(void)
-{
-    Param_t *new_paramt = (Param_t *) malloc(sizeof(Param_t));
-    new_paramt->inputRedirect = NULL;
-    new_paramt->outputRedirect = NULL;
-    new_paramt->argumentCount = 0;
-    new_paramt->background = 0;
-    printf("new_pramt->background = %d",new_paramt->background);
-    //new_paramt->inputRedirect = (char *) malloc(sizeof(char) * Buff);
-    //new_paramt->outputRedirect = (char *) malloc(sizeof(char) * Buff);
-    
-    //new_paramt->argumentVector = (char *) malloc(sizeof(char *) * MAXARGS);
-    
+
+void printParams(Param_t *param) {
     int i;
-    for(i=0; i < MAXARGS; i++)
-    {
-      new_paramt->argumentVector[i] = NULL;
-    }  
-
-    return new_paramt; 
-}
-*/
-
-void printParams(Param_t *param)
-{
-int i;
-printf ("InputRedirect: [%s]\n",
- (param->inputRedirect != NULL) ? param->inputRedirect:"NULL");
-printf ("OutputRedirect: [%s]\n",
- (param->outputRedirect != NULL) ? param->outputRedirect:"NULL");
-printf ("Background: [%d]\n", param->background);
-printf ("ArgumentCount: [%d]\n", param->argumentCount);
-for (i = 0; i < param->argumentCount; i++)
-printf("ArgumentVector[%2d]: [%s]\n", i, param->argumentVector[i]);
-}
-
-void run_loop(Param_t *params)
-{
-char *line;
-char **args;
-int status;
-
-do {
-   printf("> ");
-   line = read_line();
-   args = split_line(line, params);
-
-   free(line);
-   free(args);
- } while(status);
-}
-
-char *read_line(void) {
-  int bufsize = Buff;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
-
-  if(!buffer) {
-    fprintf(stderr, "allocation err\n");
-    exit(EXIT_FAILURE);
-  }
-
-while(1) {
-  c = getchar();
-
-  if(c == EOF || c == '\n') {
-    buffer[position] = '\0';
-    return buffer;
-  } 
-  else {
-    buffer[position] = c;
-  }
-  position++;
-
-  if(position >= bufsize) {
-    bufsize += Buff;
-    buffer = realloc(buffer, bufsize);
-    if (!buffer) {
-      fprintf(stderr, "allocation error\n");
-      exit(EXIT_FAILURE);
-      }
-   }
-  }
- }
-
-char **split_line(char *line, Param_t *params)
-{
- int bufsize = tok_Bufsize, position = 0;
- char **tokens = malloc(bufsize * sizeof(char*));
- char *token;
-
- if(!tokens)
- {
-  fprintf(stderr, "allocation error\n");
-  exit(EXIT_FAILURE);
- }
-  token = strtok(line, " \t\r\n\a");
-  while(token != NULL)
-  {
-   //gonna need to strcpy
-   tokens[position] = token;
-   position++;
-
-   if(position >= bufsize)
-   {
-    bufsize += tok_Bufsize;
-    tokens = realloc(tokens, bufsize * sizeof(char*));
-    if(!tokens)
-    {
-     fprintf(stderr, "allocation error\n");
-     exit(EXIT_FAILURE);
+    printf ("InputRedirect: [%s]\n",
+    (param->inputRedirect != NULL) ? param->inputRedirect:"NULL");
+  
+    printf ("OutputRedirect: [%s]\n",
+    (param->outputRedirect != NULL) ? param->outputRedirect:"NULL");
+  
+    printf ("Background: [%d]\n", param->background);
+  
+    printf ("ArgumentCount: [%d]\n", param->argumentCount);
+  
+    for (i = 0; i < param->argumentCount; i++) {
+      printf("ArgumentVector[%2d]: [%s]\n", i, param->argumentVector[i]);
     }
-   }
-   
-   token = strtok(NULL, " \t\r\n\a");
-  }
-  params->argumentCount = position;
-  tokens[position] = NULL;
-  return tokens;
 }
+
+char *readLine() {
+    const char t;
+    FILE *in = stdin;
+
+    char *str = malloc(sizeof(char) * 32);
+    if (str == NULL) fprintf(stderr, "MEMORY ALLOCATION FAILED\n");
+
+    fgets(str,32,in);
+
+    /*if (fgets(str, 32, in) != NULL) {
+        puts(str);
+    }
+    */
+
+    return str;
+}
+
+void parseStr(char *str, Param_t *param) {
+    char *token;
+    const char delimiter[] = " ";
+
+    token = strtok(str,delimiter);
+    if (token[0] == '\n') {
+        printf("Re enter string\n");
+        str = readLine();
+        parseStr(str, param);
+        return;
+    }
+
+    while (token != NULL) {
+        if (token[0] == '>') {
+            param->outputRedirect = token;
+            //printf("param->outputRedirect = %s\n",param->outputRedirect);
+        }
+        else if (token[0] == '<') {
+            param->inputRedirect = token;
+            //printf("param->inputRedirect = %s\n",param->inputRedirect);
+        }
+        else if (token[0] == '&') { // MAKE SURE TO CHECK ABOUT BEING LAST CHARACTER
+            param->background = 1;
+            //printf("param->background = %d\n",param->background);
+        }
+        else {
+            param->argumentVector[param->argumentCount] = token;
+            //printf("param->ArgumentVector[%d] = %s\n",param->argumentCount, param->argumentVector[param->argumentCount]);
+            param->argumentCount++;
+        }
+
+        token = strtok(NULL, delimiter);
+    }
+}
+
