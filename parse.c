713a2da17bef0d1d347d9a7fb3a4d9dd1c94ec13
jww46 <jww46@students.uwf.edu>
2015-09-17 22:11:12 -0500

    updated complete

--- parse.c
+++ parse.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "parse.h"
 
 void printParams(Param_t *param) {
@@ -28,48 +30,88 @@ char *readLine() {
     char *str = malloc(sizeof(char) * 100);
     if(str == NULL)
 		fprintf(stderr, "MEMORY ALLOCATION FAILED\n");
-
-    fgets(str, 100, in);
-        
-    /*if (fgets(str, 32, in) != NULL) {
-        puts(str);
-    }
-    */
-
+	fgets(str, 100, in);
+	size_t length = strlen(str);
+	if(length == 1)
+		return str;
+	if(str[length - 1] == '\n')
+		str[length - 1] = '\0';
+	
     return str;
 }
-
 void parseStr(char *str, Param_t *param) {
-    char *token;
-    const char delimiter[] = " ";
+	char *token;
+        const char delimiter[] = " ";
 
-    token = strtok(str,delimiter);
-    if (token[0] == '\n') {
-        printf("Re enter string\n");
-        str = readLine();
-        parseStr(str, param);
-        return;
-    }
-
-    while (token != NULL) {
-        if (token[0] == '>') {
-            param->outputRedirect = &token[1];
-            //printf("param->outputRedirect = %s\n",param->outputRedirect);
-        }
-        else if (token[0] == '<') {
-            param->inputRedirect = &token[1];
-            //printf("param->inputRedirect = %s\n",param->inputRedirect);
-        }
-        else if (token[0] == '&') { // MAKE SURE TO CHECK ABOUT BEING LAST CHARACTER
-            param->background = 1;
-            //printf("param->background = %d\n",param->background);
-        }
-        else {
-            param->argumentVector[param->argumentCount] = token;
-            //printf("param->ArgumentVector[%d] = %s\n",param->argumentCount, param->argumentVector[param->argumentCount]);
-            param->argumentCount++;
+        token = strtok(str,delimiter);
+        if (token[0] == '\n') {
+        	printf("Re enter string\n");
+        	str = readLine();
+        	parseStr(str, param);
+        	return;
         }
 
-        token = strtok(NULL, delimiter);
+        while (token != NULL) {
+
+        	if (token[0] == '>') {
+            		param->outputRedirect = &token[1];
+            		//printf("param->outputRedirect = %s\n",param->outputRedirect);
+                }	
+        	else if (token[0] == '<') {
+                	param->inputRedirect = &token[1];
+            		//printf("param->inputRedirect = %s\n",param->inputRedirect);
+        		}
+        	else if (token[0] == '&') { // MAKE SURE TO CHECK ABOUT BEING LAST CHARACTER
+            		param->background = 1;
+            		//printf("param->background = %d\n",param->background);
+        	}
+       		else 
+        	{
+			param->argumentVector[param->argumentCount] = token;
+                	//printf("param->ArgumentVector[%d] = %s\n",param->argumentCount, param->argumentVector[param->argumentCount]);
+                	param->argumentCount++;
+		}
+        	token = strtok(NULL, delimiter);
     }
 }
+
+int launch(Param_t *param)
+{
+	pid_t pid;
+        int status;
+ 
+       	pid = fork();	
+        if(pid == 0){
+		if(param->inputRedirect){
+                        FILE *fin = freopen(param->inputRedirect, "r", stdin);
+                                if(!(fin)){
+                                        fprintf(stdout, "Error! Input redirection failed.");
+                                }
+                }
+
+		if(param->outputRedirect){
+			FILE *fout = freopen(param->outputRedirect, "w", stdout);
+				if(!(fout)){
+					fprintf(stdout, "Error! Output redirection failed.");
+				}
+		}
+		execvp(param->argumentVector[0], param->argumentVector);
+	}
+	else if(pid < 0){
+		fprintf(stdout, "Error! Process fork failed.");
+	}
+	else{
+
+		if(param->background != 1){
+			waitpid(pid, &status, 0);
+			printf("DONE WITH BACKGROUND\n");
+		}
+		 else {
+         	    waitpid(pid, &status, WNOHANG);
+   	            printf("DONE WITH BACKGROUND\n");
+		}
+	}	
+	return 0;
+		
+}
+
